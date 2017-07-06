01ad2620a0c6b05997d9425e6a56b1e1103f8a56
Risenwilliams <williamsrising592@gmail.com>
2017-07-06 14:41:30 +0100

    Added the Fragment for the units in converter Activity

--- app/src/main/java/myapplication/risenapps/com/studentutility/Converter.java
+++ app/src/main/java/myapplication/risenapps/com/studentutility/Converter.java
@@ -1,15 +1,35 @@
 package myapplication.risenapps.com.studentutility;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 //This is the Converter activity
-public class Converter extends Activity {
+public class Converter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter); //Here it calls its xml file
+
+        Button units = (Button)findViewById(R.id.Units);
+        units.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+                ft1.add(R.id.units,new unitsFragment());
+                ft1.commit();
+
+            }
+        });
+
+
+
     }
 
 }
--- app/src/main/java/myapplication/risenapps/com/studentutility/unitsFragment.java
+++ app/src/main/java/myapplication/risenapps/com/studentutility/unitsFragment.java
@@ -1,7 +1,6 @@
 package myapplication.risenapps.com.studentutility;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,18 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-
-
-public class moreUnits extends Fragment {
+public class unitsFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_more_units, container, false);
-
+        return inflater.inflate(R.layout.fragment_units, container, false);
     }
-
 
 }
